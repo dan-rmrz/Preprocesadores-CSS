d761950d6ef547f436fdaaa98937b3ba9fcbedd7
Daniel Ramirez <danxur@live.com.mx>
2020-03-23 17:57:31 -0600

    Configuracion inicial e instalacion de herramientas: React, ReactDOM, Babel, WebPack y pug

---
+++ src/components/pug/Componenteprueba.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Componenteprueba = () => pug`
+  div
+    h1 Si funciona...
+    h2 si jala
+`;
+
+export default Componenteprueba
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Componenteprueba from './components/pug/Componenteprueba';
+
+const container = document.getElementById('app');
+ReactDOM.render(<Componenteprueba />, container);
