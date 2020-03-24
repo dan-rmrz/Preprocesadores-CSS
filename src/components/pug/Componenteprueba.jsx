f4aa71b47fcb79a7d007672f71c2d42bf891d9a0
Daniel Ramirez <danxur@live.com.mx>
2020-03-24 03:04:59 -0600

    Fin del modulo de pug

--- src/components/pug/Componenteprueba.jsx
+++ src/components/pug/Componenteprueba.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import '../../assets/styles/pug/Landing.css';
+import imagen from '../../assets/static/imagen.png';
 
-const Componenteprueba = () => pug`
-  div
-    h1 Si funciona...
-    h2 si jala
-`;
+const Componenteprueba = () => (
+	<>
+		<header>
+			<h1>PlatziGames</h1>
+			<a href="" className="boton">Registro</a>
+		</header>      
+
+		<section className="intro">
+			<div className="intro__imagen">
+				<img src={imagen} alt="imagen"/>
+			</div>
+			<div className="intro__contenido">
+				<h2>Titulo Principal</h2>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					 Adipisci delectus unde tempora modi ipsam officia explicabo 
+					 quibusdam! Iste, cumque nihil. Voluptatibus tempora temporibus 
+					 vitae? Maxime cum saepe cupiditate possimus ut?
+					 <span>
+						 Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+						 Magnam repellendus obcaecati, unde in explicabo ut! Atque 
+						 officia architecto esse laboriosam, non provident fugiat, 
+						 tempora aliquid excepturi necessitatibus sint, earum illum?
+						</span>
+				</p>
+			</div>
+		</section>
+	</>
+);
 
 export default Componenteprueba
